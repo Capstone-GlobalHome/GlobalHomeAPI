531e02d743630732175b271fe0b5f8d6b315f0ab
Harpreet Singh <harpreet.iosdev@gmail.com>
2020-06-05 15:35:16 +0530

    helper methods added for login, signup and checkuser

--- utilis/helper.js
+++ utilis/helper.js
@@ -4,6 +4,73 @@ var config = require("../config/config");
 const _ = require("lodash");
 var uuid = require("uuid");
 const moment = require("moment-timezone");
+const fs = require("fs");
+const faker = require("faker");
+console.log(__dirname);
+var userdb = JSON.parse(fs.readFileSync(__dirname + "/users.json", "UTF-8"));
+
+//Auth
+exports.isAuthenticated = async function (email, password) {
+  console.log("isAuthenticated called");
+  let id = undefined;
+  let name = "";
+  let res = false;
+  await userdb.users.findIndex((user) => {
+    if (user.email === email && user.password === password) {
+      id = user.id;
+      name = user.fullname;
+      res = true;
+    }
+  });
+  return { data: id, name: name, email: email, status: res };
+};
+
+//New User
+exports.postUser = async function (fullname, email, password) {
+  console.log("postUser called");
+  let exist = false;
+  await userdb.users.findIndex((user) => {
+    if (user.email === email) {
+      exist = true;
+    }
+  });
+
+  if (!exist) {
+    const id = await faker.random.number();
+    console.log("new id is", id);
+    const newUser = {
+      id: id,
+      fullname: fullname,
+      email: email,
+      password: password,
+      createdAt: Date.now(),
+      updatedAt: Date.now(),
+    };
+    await userdb.users.push(newUser);
+    fs.writeFileSync(__dirname + "/users.json", JSON.stringify(userdb, null, 2), "utf8");
+    return { data: id, name: fullname, email: email, status: true };
+  } else {
+    console.log("user exists");
+    return { status: false };
+  }
+};
+
+//Check User
+exports.checkUserExist = async function (email) {
+  console.log("checkUserExist called");
+  let exist = false;
+  await userdb.users.findIndex((user) => {
+    if (user.email === email) {
+      exist = true;
+    }
+  });
+
+  if (!exist) {
+    return { status: false };
+  } else {
+    return { status: true };
+  }
+};
 
 //validateToken
 exports.tokenVerify = function (req, res, next) {
