c515c198e741cb808419c0a0b3e9f6bec51fd3b6
Harpreet Singh <harpreet.iosdev@gmail.com>
2020-06-07 01:09:13 +0530

    helper methods added for resend, verify and add verification code

--- utilis/helper.js
+++ utilis/helper.js
@@ -7,29 +7,31 @@ const moment = require("moment-timezone");
 const fs = require("fs");
 const faker = require("faker");
 console.log(__dirname);
-var userdb = JSON.parse(fs.readFileSync(__dirname + "/users.json", "UTF-8"));
+var usertb = JSON.parse(fs.readFileSync(__dirname + "/users.json", "UTF-8"));
+var verificaitontb = JSON.parse(fs.readFileSync(__dirname + "/verifications.json", "UTF-8"));
 
 //Auth
 exports.isAuthenticated = async function (email, password) {
   console.log("isAuthenticated called");
   let id = undefined;
-  let name = "";
+  let userObj;
   let res = false;
-  await userdb.users.findIndex((user) => {
+  await usertb.users.findIndex((user) => {
     if (user.email === email && user.password === password) {
       id = user.id;
-      name = user.fullname;
       res = true;
+      userObj = user;
     }
   });
-  return { data: id, name: name, email: email, status: res };
+  console.log("userObj", userObj);
+  return { data: userObj, status: res };
 };
 
 //New User
 exports.postUser = async function (fullname, email, password) {
   console.log("postUser called");
   let exist = false;
-  await userdb.users.findIndex((user) => {
+  await usertb.users.findIndex((user) => {
     if (user.email === email) {
       exist = true;
     }
@@ -37,20 +39,79 @@ exports.postUser = async function (fullname, email, password) {
 
   if (!exist) {
     const id = await faker.random.number();
-    console.log("new id is", id);
     const newUser = {
       id: id,
       fullname: fullname,
       email: email,
       password: password,
+      status: "Pending_verificaiton",
       createdAt: Date.now(),
       updatedAt: Date.now(),
     };
-    await userdb.users.push(newUser);
-    fs.writeFileSync(__dirname + "/users.json", JSON.stringify(userdb, null, 2), "utf8");
+    await usertb.users.push(newUser);
+    fs.writeFileSync(__dirname + "/users.json", JSON.stringify(usertb, null, 2), "utf8");
     return { data: id, name: fullname, email: email, status: true };
   } else {
     console.log("user exists");
+    return { status: false };
+  }
+};
+
+//Add Verificaiton code
+exports.addVerificationCode = async function (id, email, code) {
+  console.log("addVerificationCode called");
+  let exist = false;
+  await verificaitontb.codes.findIndex((verification) => {
+    if (verification.email === email) {
+      exist = true;
+    }
+  });
+
+  if (!exist) {
+    const newVerification = {
+      id: id,
+      email: email,
+      code: code,
+    };
+    await verificaitontb.codes.push(newVerification);
+    fs.writeFileSync(__dirname + "/verifications.json", JSON.stringify(verificaitontb, null, 2), "utf8");
+    return { status: true };
+  } else {
+    console.log("verification already exists");
+    return { status: false };
+  }
+};
+
+//Verify Verification code
+exports.verify = async function (req, verificationCode) {
+  console.log(req.userId);
+  console.log(req.email);
+  let verified = false;
+  let data;
+  await verificaitontb.codes.findIndex((verification) => {
+    if (verification.id === req.userId) {
+      console.log("user matched", verification.code);
+      console.log("verificationCode is", verificationCode);
+      if (verification.code == verificationCode) {
+        verified = true;
+      }
+    }
+  });
+
+  if (verified) {
+    //Update user status to Active
+    await usertb.users.findIndex((user) => {
+      if (user.email === req.email && user.id === req.userId) {
+        console.log("user matched now -update it");
+        user.status = "Pending_verificaiton" ? "Active" : "Active";
+        data = user;
+        status = true;
+      }
+    });
+    fs.writeFileSync(__dirname + "/users.json", JSON.stringify(usertb, null, 2), "utf8");
+
+    return { data: data, status: true };
+  } else {
     return { status: false };
   }
 };
@@ -59,7 +120,7 @@ exports.postUser = async function (fullname, email, password) {
 exports.checkUserExist = async function (email) {
   console.log("checkUserExist called");
   let exist = false;
-  await userdb.users.findIndex((user) => {
+  await usertb.users.findIndex((user) => {
     if (user.email === email) {
       exist = true;
     }
@@ -72,11 +133,31 @@ exports.checkUserExist = async function (email) {
   }
 };
 
+//Resend verification code
+exports.resend = async function (req, verificationCode) {
+  let verified = false;
+  //Update user status to Active
+  await verificaitontb.codes.findIndex((verification) => {
+    if (verification.email === req.email && verification.id === req.userId) {
+      console.log("user matched now -update verification code");
+      verification.code = verificationCode;
+      verified = true;
+    }
+  });
+  fs.writeFileSync(__dirname + "/verifications.json", JSON.stringify(verificaitontb, null, 2), "utf8");
+
+  if (verified) {
+    return { status: true };
+  } else {
+    return { status: false };
+  }
+};
+
 //validateToken
 exports.tokenVerify = function (req, res, next) {
   jwt.verify(req.headers["x-access-token"], process.env.API_KEY, function (err, authData) {
     req.userId = err ? null : authData.id;
-    req.username = err ? null : authData.username;
+    req.email = err ? null : authData.email;
     req.jwtError = err ? err : null;
     next();
   });
@@ -111,11 +192,11 @@ exports.HTTPError = function (statusCode, message) {
   return error;
 };
 
-exports.createTokens = function (userId, username, secret, secret2) {
+exports.createTokens = function (userId, email, secret, secret2) {
   // return createTokens(user, secret,secret2);
-  const createToken = jwt.sign({ id: userId, username: username }, secret, { expiresIn: config.jwtExpiryTime });
+  const createToken = jwt.sign({ id: userId, email: email }, secret, { expiresIn: config.jwtExpiryTime });
 
-  const createRefreshToken = jwt.sign({ id: userId, username: username }, secret2, { expiresIn: config.refreshExpiryTime });
+  const createRefreshToken = jwt.sign({ id: userId, email: email }, secret2, { expiresIn: config.refreshExpiryTime });
 
   var newToken = {
     token: createToken,
@@ -125,10 +206,10 @@ exports.createTokens = function (userId, username, secret, secret2) {
   return newToken;
 };
 
-async function createTokens(userId, username, secret, secret2) {
-  const createToken = jwt.sign({ id: userId, username: username }, secret, { expiresIn: config.jwtExpiryTime });
+async function createTokens(userId, email, secret, secret2) {
+  const createToken = jwt.sign({ id: userId, email: email }, secret, { expiresIn: config.jwtExpiryTime });
 
-  const createRefreshToken = jwt.sign({ id: userId, username: username }, secret2, { expiresIn: config.refreshExpiryTime });
+  const createRefreshToken = jwt.sign({ id: userId, email: email }, secret2, { expiresIn: config.refreshExpiryTime });
   var newToken = {
     token: createToken,
     refreshToken: createRefreshToken,
