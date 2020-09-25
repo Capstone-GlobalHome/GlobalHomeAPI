7bc4b63d2a189e64d702f60921dc9492f8edb365
Rajinder <dev.rajinder87@gmail.com>
2020-09-25 14:46:17 +0530

    Commit server setup

--- config/config.js
+++ config/config.js
@@ -10,19 +10,20 @@ module.exports = {
     "logging": false
   },
   "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql",
-    "operatorsAliases": false
+    "username": environment.DB_USERNAME,
+    "password": environment.DB_PASSWORD,
+    "database": environment.DB_NAME,
+    "host": environment.DB_HOST,
+    "dialect": environment.DB_CONNECTION,
+    "operatorsAliases": 1,
+    "logging": false
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql",
+    "username": environment.DB_USERNAME,
+    "password": environment.DB_PASSWORD,
+    "database": environment.DB_NAME,
+    "host": environment.DB_HOST,
+    "dialect": environment.DB_CONNECTION,
     "operatorsAliases": 1,
     "logging": false
   }
