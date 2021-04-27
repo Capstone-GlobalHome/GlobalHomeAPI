b5a74681f230667176657bdeb7adb975e43fb43c
Rajinder <dev.rajinder87@gmail.com>
2021-04-27 18:50:41 +0530

    Sensor data for VOC updated

--- src/constants/sensor.ranges.js
+++ src/constants/sensor.ranges.js
@@ -3,13 +3,13 @@ export const SENSOR_RANGES = {
         "0-1000": "GOOD",
         "1001-5000": "POOR",
         "5001-40000": "WARNINGS",
-        "400001-Infinity": "DANGER"
+        "40001-Infinity": "DANGER"
     },
     "SENSOR_VOC": {
         "0-300": "LOW",
         "301-500": "ACCEPTABLE",
         "501-1000": "MARGINAL",
-        "10001-Infinity": "HIGH"
+        "1001-Infinity": "HIGH"
     },
     "SENSOR_HUMIDITY": {
         "0-30": "DRY",
